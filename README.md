2f7b0b98cbb8c329a1e18cb5e95231a297f62f2a
yutaka-matsuzawa <yutaka.matsuzawa@gree.net>
2016-08-14 21:00:12 +0900

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# forest_monitor
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'forest_monitor'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures forest_monitor'
+long_description 'Installs/Configures forest_monitor'
+version '0.1.0'
